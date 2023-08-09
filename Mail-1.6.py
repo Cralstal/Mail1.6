c6f1b33ab8710fb53e01303c3733d7dc14e34ba4
Mr.Paing-Gyi <113289641+Cralstal@users.noreply.github.com>
2023-08-09 15:49:16 +0630

    Add files via upload

---
+++ Mail-1.6.py
@@ -0,0 +1,7 @@
+import os,platform
+os.system('git pull') 
+bit=platform.architecture()[0]
+if bit=="32bit":
+    print('Sorry 32 Bit Not Supported...')
+elif bit=="64bit":
+     __import__("Cyber")
